3bda0b8ae4e322f5acda3bea7ce66ca5cc0b0ce5
bk-tripathi <balkrishna.tri09@gmail.com>
2023-05-26 03:55:19 +0530

    fix git issue changes

--- src/features/product/feed/Product.tsx
+++ src/features/product/feed/Product.tsx
@@ -17,42 +17,24 @@ const Product: React.FC<ProductProps> = ({ product }) => {
   const router = useRouter();
 
   return (
-    <>
-      <motion.div
-        className="w-3/4 bg-[#1a1a2c] rounded-lg shadow-md flex flex-col
+    <motion.div
+      className="w-3/4 bg-[#1a1a2c] rounded-lg shadow-md flex flex-col
       transition-all duration-200 transform hover:scale-150 hover:shadow-lg  cursor-pointer"
-        initial="initial"
-        animate="in"
-        exit="out"
-        variants={pageZoom}
-        transition={pageTransition}
-        layout
-        onClick={async () => {
-          await loadProduct(product.id);
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageZoom}
+      transition={pageTransition}
+      layout
+      onClick={async () => {
+        await loadProduct(product.id);
 
-<<<<<<< HEAD
-          if (selectedProduct?.id !== product.id) {
-            removeSelectedProduct();
-            await loadProduct(product.id);
-          }
-
-          router.push(`/product/${product.id}`);
-        }}
-      >
-        <ProductImage product={product} />
-        <ProductInfo product={product} />
-      </motion.div>
-    </>
-=======
         router.push(`/product/${product.id}`);
       }}
-      className="w-3/4 bg-[#1a1a2c] rounded-lg shadow-md flex flex-col
-      transition-all duration-500 ease-in-out transform bg-center bg-cover"
     >
       <ProductImage product={product} />
       <ProductInfo product={product} />
     </motion.div>
->>>>>>> 2d8b55fee629de3bfade820b22035a0ab0d3286c
   );
 };
 
