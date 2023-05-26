381524f0e471a782d2832c35b3ef22a65321a2b7
bk-tripathi <balkrishna.tri09@gmail.com>
2023-05-26 11:36:50 +0530

    cart

--- src/components/sidebar/SidebarLogo.tsx
+++ src/components/sidebar/SidebarLogo.tsx
@@ -8,19 +8,18 @@ const SidebarLogo: React.FC<SidebarLogoProps> = () => {
 
   return (
     // eslint-disable-next-line @next/next/no-img-element
-   
+
     <img
       loading="lazy"
-      src="/icons/icon.jpg"
+      src="/icons/icon.png"
       alt="icon"
       onClick={toggleSidebar}
-      className={`w-10 sm:w-8 z-20 transition-all duration-200
+      className={` w-36 sm:w-16 z-20 transition-all duration-200
       transform translate-x-20 sm:translate-x-0 p-2 sm:p-0
       rounded-full bg-white shadow-md sm:shadow-none ${
         sidebarActive && "!translate-x-0 !shadow-none"
       }`}
     />
-  
   );
 };
 
